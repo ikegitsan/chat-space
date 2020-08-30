8edad23e5d6e76662b53534e99cb850bbe06f519
ikegitsan <ssm.no99@gmail.com>
2020-08-30 17:27:49 +0900

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  
+  devise_for :users
+  # 下の行は削除する
+  # get 'messages/index
   root "messages#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:edit, :update]
 end
