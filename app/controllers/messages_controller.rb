45a0ef8cd3ecaab4990048efe3ef97ddfe7b4c6b
ikegitsan <ssm.no99@gmail.com>
2020-09-13 17:47:30 +0900

    eighth commit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,13 +7,11 @@ class MessagesController < ApplicationController
   end
 
   def create
-    # binding.pry
     @message = @group.messages.new(message_params)
     if @message.save
       respond_to do |format|
         format.json
       end
-      # redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
